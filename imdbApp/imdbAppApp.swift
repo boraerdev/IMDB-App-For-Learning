878d7679d14c50250d8404a2b773696c85cde5c0
Bora Erdem <boraerdev@gmail.com>
2022-07-08 14:52:46 +0300

    Initial Commit

---
+++ imdbApp/imdbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  imdbAppApp.swift
+//  imdbApp
+//
+//  Created by Bora Erdem on 8.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct imdbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
